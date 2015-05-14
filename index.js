c559e0227629638b0ad51d3123f441fae40d4cbd
Cameron Lazell <cameron.lazell@lisasoft.com>
2015-05-14 17:06:29 +1000

    Working with disconects

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+  socket.on('disconnect', function () {
+    io.emit('user disconnected');
+  });
 });
 
 http.listen(process.env.PORT || 3000, function(){
